6169248ab750796fc4aa39d30d7ee88fa02324ca
“[Maulana <[maulana.nurhendronoto@ui.ac.id]>
2021-09-14 21:02:58 +0700

    modul 1 jmart revisi

--- MaulanaNurhendronotoJmartAK/Jmart.java
+++ MaulanaNurhendronotoJmartAK/Jmart.java
@@ -5,11 +5,11 @@ package MaulanaNurhendronotoJmartAK;
  * JMart Praktikum Modul 1
  *
  * @author Maulana Nurhendronoto (2006577542)
- * @version 1.0
+ * @version 2.0
  */
 public class Jmart
 {
-    public static void main(String args[]){ //yet to be filled
+    public static void main(String args[]){ 
         
     }
     
@@ -21,34 +21,36 @@ public class Jmart
         return "oop";
     }
     
-    public static float getDiscountedPercentage(int before, int after){
+    public static float getDiscountPercentage(int before, int after){
         if(before < after)
         {
             return 0.0f;
         }
-        else return (float) before/(before - after);
+        int cut = before - after;
+        return (100.0f * cut)/before;
     }
     
     public static int getDiscountedPrice(int price, float discountPercentage){
-        if(discountPercentage > 100.0f){
+        if(discountPercentage >= 100.0f){
             return 0;
         }
-        else return price - (price * (int) discountPercentage);
+        else return price - (int) ((price *  discountPercentage)/100.0f);
     }
     
     public static int getOriginalPrice(int discountedPrice, float discountPercentage){
-        return (100/(100-(int)discountPercentage))*discountedPrice;
+        float divider = 100.0f - discountPercentage;
+        return (int) (100 * discountedPrice/divider);
     }
     
     public static float getCommissionMultiplier(){
         return 0.05f;
     }
     
-    public static int getAdjustedPrice(int price){ //error data type conversion
-        return price + (price*0.05);
+    public static int getAdjustedPrice(int price){
+        return  price + (int) (price * 0.05);
     }
     
-    public static int getAdminFee(int price){ //error data type conversion
-        return 0.05*price;
+    public static int getAdminFee(int price){
+        return (int) (0.05 * price);
     }
 }
