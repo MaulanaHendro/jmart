63b50f733400f1a6d452c8bd800022472364dc80
“[Maulana <[maulana.nurhendronoto@ui.ac.id]>
2021-11-20 10:56:23 +0700

    CS Praktikum Modul 8

--- src/main/java/com/MaulanaNurhendronotoJmartAK/controller/AccountController.java
+++ src/main/java/com/MaulanaNurhendronotoJmartAK/controller/AccountController.java
@@ -1,16 +1,37 @@
 package com.MaulanaNurhendronotoJmartAK.controller;
 
 import com.MaulanaNurhendronotoJmartAK.Account;
+import com.MaulanaNurhendronotoJmartAK.dbjson.JsonTable;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/account")
-
-public class AccountController
+public abstract class AccountController implements BasicGetController<Account>
 {
+	public static final String REGEX_EMAIL = "^\\w+([\\~*_&.]?\\w+)*@\\w+([\\.-]?\\w+)*.?\\w+$";
+	public static final String REGEX_PASSWORD = "(?=.*[a-z])(?=.*[A-Z])(?=\\\\S+$).{8,}$";
+	public static final Pattern REGEX_PATTERN_EMAIL = Pattern.compile(REGEX_EMAIL);
+	public static final Pattern REGEX_PATTERN_PASSWORD = Pattern.compile(REGEX_PASSWORD);
+	public static JsonTable<Account> accountTable;
+	
 	@GetMapping
 	String index() { return "account page"; }
 	
+	 @PostMapping("/account/login")
+     @ResponseBody Account login(String email, String password)
+     {
+		 for(Account each : accountTable)
+		 {
+			 if(each.email == email && each.password == password)
+			 {
+				 return each;
+			 }
+		 }
+		 return null;
+     }
+   
 	@PostMapping("/register")
 	Account register
 	(
@@ -22,8 +43,19 @@ public class AccountController
 		return new Account(name, email, password);
 	}
 	
+	@Override
 	@GetMapping("/{id}")
-	String getById(@PathVariable int id) { return "account id " + id + " not found!"; }
+	public Account getById(@PathVariable int id)
+	{
+		return getJsonTable().get(id);
+	}
+	
+	@Override
+	@GetMapping("/page")
+	public List<Account> getPage(int page, int pageSize)
+	{
+		return getJsonTable().subList(page, pageSize);
+	}
 }
 
 
