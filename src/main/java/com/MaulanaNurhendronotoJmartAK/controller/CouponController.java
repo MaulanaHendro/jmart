ed7399f2c7dc05afab16f067dd4ba3e63cc2612f
“[Maulana <[maulana.nurhendronoto@ui.ac.id]>
2021-11-22 21:09:08 +0700

    Post Test Praktikum Modul 8

---
+++ src/main/java/com/MaulanaNurhendronotoJmartAK/controller/CouponController.java
@@ -0,0 +1,49 @@
+package com.MaulanaNurhendronotoJmartAK.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.MaulanaNurhendronotoJmartAK.Coupon;
+import com.MaulanaNurhendronotoJmartAK.dbjson.JsonAutowired;
+import com.MaulanaNurhendronotoJmartAK.dbjson.JsonTable;
+
+@RestController
+@RequestMapping("/coupon")
+public class CouponController implements BasicGetController<Coupon>
+{
+	@JsonAutowired(value = Coupon.class, filepath = "C:\\Users\\USER\\Documents\\Hendro\\Java Programming BlueJ\\jmart\\coupon.json")
+	public static JsonTable<Coupon> couponTable;
+	
+	public JsonTable<Coupon> getJsonTable()
+	   {
+	       return couponTable;
+	   }
+	
+	@GetMapping("/{id}/isUsed")
+	public boolean isUsed(int id)
+	{
+		for(Coupon coupon : getJsonTable())
+		{
+			if(coupon.id == id)
+			{
+				return coupon.isUsed();
+			}
+		}
+		return false;
+	}
+	
+	/*@GetMapping("/{id}/canApply")
+	public boolean canApply(@RequestParam int id, @RequestParam double price, @RequestParam double discount)
+	{
+		for(Coupon coupon : getJsonTable())
+		{
+			if(coupon.id == id)
+			{
+				return coupon.canApply(price, discount);
+			}
+		}
+		return false;
+	}*/
+}
